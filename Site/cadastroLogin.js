f1de67415eb3ae8b6891aff55160b4351fc1e1fd
LUIZ HENRIQUE RIGO GRIEBELER <luiz_griebeler@estudante.sc.senai.br>
2022-06-23 21:40:03 -0300

    Criado tela para se cadastrar

---
+++ Site/cadastroLogin.js
@@ -0,0 +1,21 @@
+function cadastrarLogin(){
+
+  var nomeUsuario = document.getElementById('nomeUsuario').value
+  var sobrenomeUsuario = document.getElementById('sobrenomeUsuario').value
+  var user = document.getElementById('user').value
+  var password = document.getElementById('password').value
+  var password2 = document.getElementById('password2').value
+  var compara = new Boolean(true);
+
+  if ((password == '') || (password2 == '') || (nomeUsuario == '') || (sobrenomeUsuario == '') || (user == '')){
+    alert("Nenhuma das senhas podem ficar sem preencher!");
+    return false
+  }else if ((compara == true) && (password == password2)) {
+    window.location.href = "login.html"
+    alert("Cadastrado com sucesso!");
+  }
+  else {
+    alert("As senhas devem ser iguais.");
+  }
+
+}
